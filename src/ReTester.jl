2c77b7d7a3fe620c82281ad72a43c4a108116286
Sam Anklesaria <sam@spacemaker.ai>
2020-10-10 17:50:10 -0400

    Revise docstring for proposed workflow

--- src/ReTester.jl
+++ src/ReTester.jl
@@ -1,21 +1,18 @@
 """
-Put your tests in a separate `Tests` package inside your main package.
-Add your main package to your `Tests` package using `Pkg.dev`. 
-Write a `runtests.jl` file in the main package directory like this:
+Set your `test/runtests.jl` file to be 
 ```
 using Revise
 using ReTester
-entr(run_tests(Tests.test), ["src", "Tests/src"]; pause=0.2)
+includet(\"tests.jl\")
+entr(run_tests(test), ["src"]; pause=0.2, all=true)
 ```
-To run tests, in the main directory, run 
-`julia --project=Tests runtests.jl`
-If you only want to run the tests named foo and bar, run
-`julia --project=Tests runtests.jl foo bar`
+and create a `tests.jl` file with your real test function.
+Your test will get run whenever you change a file. 
 """
 module ReTester
 using Test
 
-export run_tests, @do_test
+export run_tests, @test, @testset
 
 function run_tests(test_f::Function)
   ()-> begin
@@ -28,16 +25,5 @@ function run_tests(test_f::Function)
     println("") 
   end
 end
-
-"Just like `@testset`, but allows you to filter out which tests get run
-by passing them in `ARGS`"
-macro do_test(name, block)
-  return :((isempty(ARGS) || $name in ARGS) && @testset($name, $block))
-end
-
-# TODO: the test function is getting called multiple times
-# TODO: do_test does not work
-# TODO: add --debug flag support for calling `@enter f` instead of just `f`
-# TODO: add a way to stop on the first error
 
 end # module
